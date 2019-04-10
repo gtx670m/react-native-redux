196ab308e4798ddda24698f975ef3a5f11928f85
Quang Le <quang.le@MM00550.local>
2019-04-10 14:23:51 +0700

    add redux

--- index.js
+++ index.js
@@ -5,5 +5,17 @@
 import { AppRegistry } from "react-native";
 import { name as appName } from "./app.json";
 import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import React from "react";
+import allReducers from "./reducers";
 
-AppRegistry.registerComponent(appName, () => App);
+let store = createStore(allReducers);
+
+const mainApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => mainApp);
